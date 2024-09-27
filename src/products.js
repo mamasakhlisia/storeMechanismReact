27470a24c1a136dc9038b8181149f1f9453523d8
Aleksandre Mamasakhlisi <129415727+mamasakhlisia@users.noreply.github.com>
2024-09-27 14:31:46 +0400

    Add files via upload

---
+++ src/products.js
@@ -0,0 +1,74 @@
+import imageProduct1 from './assets/images/1.png' 
+import imageProduct2 from './assets/images/2.png' 
+import imageProduct3 from './assets/images/3.png' 
+import imageProduct4 from './assets/images/4.png' 
+import imageProduct5 from './assets/images/5.png' 
+import imageProduct6 from './assets/images/6.png' 
+import imageProduct7 from './assets/images/7.png' 
+import imageProduct8 from './assets/images/8.png' 
+export const products = [
+    {
+        id: 1,
+        name: 'IPhone 16 Pro Max',
+        price: 2000,
+        image: imageProduct1,
+        description: 'Mollit anim consectetur adipisicing aute pariatur ad mollit ad. Officia amet aliqua non laborum dolore sint sit eu sit sunt laboris. Dolor tempor ipsum culpa Lorem ipsum ullamco ullamco dolor sit do. Do proident quis ad laborum et proident laborum. Non voluptate eiusmod aute labore ea aute.',
+        slug: 'hertfoid-upholstered-chair'
+    },
+    {
+        id: 2,
+        name: 'Samsung Galaxy S24',
+        price: 1500,
+        image: imageProduct2,
+        description: 'Mollit anim consectetur adipisicing aute pariatur ad mollit ad. Officia amet aliqua non laborum dolore sint sit eu sit sunt laboris. Dolor tempor ipsum culpa Lorem ipsum ullamco ullamco dolor sit do. Do proident quis ad laborum et proident laborum. Non voluptate eiusmod aute labore ea aute.',
+        slug: 'bingdon-pholstered-chair-swivel'
+    },
+    {
+        id: 3,
+        name: 'Xiaomi Redmi 13',
+        price: 700,
+        image: imageProduct3,
+        description: 'Mollit anim consectetur adipisicing aute pariatur ad mollit ad. Officia amet aliqua non laborum dolore sint sit eu sit sunt laboris. Dolor tempor ipsum culpa Lorem ipsum ullamco ullamco dolor sit do. Do proident quis ad laborum et proident laborum. Non voluptate eiusmod aute labore ea aute.',
+        slug: 'jeses-minimore-modern-style-etta'
+    },
+    {
+        id: 4,
+        name: 'Google Pixel 9',
+        price: 700,
+        image: imageProduct4,
+        description: 'Mollit anim consectetur adipisicing aute pariatur ad mollit ad. Officia amet aliqua non laborum dolore sint sit eu sit sunt laboris. Dolor tempor ipsum culpa Lorem ipsum ullamco ullamco dolor sit do. Do proident quis ad laborum et proident laborum. Non voluptate eiusmod aute labore ea aute.',
+        slug: 'eses-minimore-modern-style-zakari'
+    },
+    {
+        id: 5,
+        name: 'Honor 9',
+        price: 600,
+        image: imageProduct5,
+        description: 'Mollit anim consectetur adipisicing aute pariatur ad mollit ad. Officia amet aliqua non laborum dolore sint sit eu sit sunt laboris. Dolor tempor ipsum culpa Lorem ipsum ullamco ullamco dolor sit do. Do proident quis ad laborum et proident laborum. Non voluptate eiusmod aute labore ea aute.',
+        slug: 'corrigan-studio-tufed-upholstered-wide-winback-armchair'
+    },
+    {
+        id: 6,
+        name: 'IPhone 15 Pro Max',
+        price: 1700,
+        image: imageProduct6,
+        description: 'Mollit anim consectetur adipisicing aute pariatur ad mollit ad. Officia amet aliqua non laborum dolore sint sit eu sit sunt laboris. Dolor tempor ipsum culpa Lorem ipsum ullamco ullamco dolor sit do. Do proident quis ad laborum et proident laborum. Non voluptate eiusmod aute labore ea aute.',
+        slug: 'corrigan-studio-273-wide-armchair'
+    },
+    {
+        id: 7,
+        name: 'Macbook Air M2',
+        price: 900,
+        image: imageProduct7,
+        description: 'Mollit anim consectetur adipisicing aute pariatur ad mollit ad. Officia amet aliqua non laborum dolore sint sit eu sit sunt laboris. Dolor tempor ipsum culpa Lorem ipsum ullamco ullamco dolor sit do. Do proident quis ad laborum et proident laborum. Non voluptate eiusmod aute labore ea aute.',
+        slug: 'leston-wide-upholstered-fabric'
+    },
+    {
+        id: 8,
+        name: 'IMac',
+        price: 5000,
+        image: imageProduct8,
+        description: 'Mollit anim consectetur adipisicing aute pariatur ad mollit ad. Officia amet aliqua non laborum dolore sint sit eu sit sunt laboris. Dolor tempor ipsum culpa Lorem ipsum ullamco ullamco dolor sit do. Do proident quis ad laborum et proident laborum. Non voluptate eiusmod aute labore ea aute.',
+        slug: 'stephanny-275-wide-tufted-armchair'
+    },
+];
